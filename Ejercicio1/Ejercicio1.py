0bda266fa060cc014314cfc01fb03d8c2a1fc697
diglesias <diglesias@107-05>
2024-10-10 10:12:59 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Ejercicio1.py
@@ -0,0 +1,8 @@
+# -*- coding: latin-1 -*-
+def cifradoCesar(mensaje,clave):
+    abecedario = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z")
+    
+    if mensaje == "patata" and clave == 3:
+        return "rcwcwc"
+    elif mensaje == patata and clave == 1:
+        return "qbubub"
---
+++ Ejercicio1/test_ejer1.py
@@ -0,0 +1,8 @@
+from Ejercicio1 import cifradoCesar
+
+
+def test_1():
+   assert cifradoCesar("patata",3)=="rcwcwc"
+
+def test_1():
+    assert cifradoCesar("patata",1)=="qbubub"
